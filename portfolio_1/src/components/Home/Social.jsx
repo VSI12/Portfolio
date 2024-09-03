ab5357c73549d58945dca5308456e44995bddd5a
Victor Iliya <VictorIliya15@gmail.com>
2024-09-03 15:35:09 +0100

    Created react app with Create-react-app instead of vite.

---
+++ portfolio_1/src/components/Home/Social.jsx
@@ -0,0 +1,24 @@
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="www.linkedin.com" className="home__social-icon" target="_blank">
+            <i className="uil uil-linkedin-alt"></i>
+        </a>
+
+        <a href="www.github.com" className="home__social-icon" target="_blank">
+            <i className="uil uil-github-alt"></i>
+        </a>
+
+        <a href="www.dribble.com" className="home__social-icon" target="_blank">
+            <i className="uil uil-dribbble"></i>
+        </a>
+
+        <a href="www.instagram.com" className="home__social-icon" target="_blank">
+            <i className="uil uil-instagram"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
