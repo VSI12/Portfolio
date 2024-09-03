ad6f610cfed19064b38cd06bdd2ac6fced7b1a7a
Victor Iliya <VictorIliya15@gmail.com>
2024-09-03 18:40:33 +0100

    Added the about section

--- portfolio_1/src/App.js
+++ portfolio_1/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css"
 import Header from "./components/header/header"
 import Home from "./components/Home/Home" 
+import About from "./components/About/About"
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
 
       <main className="main">
         <Home/>
+        <About/>
       </main>
     </>
   )
