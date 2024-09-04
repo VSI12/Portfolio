38b21cd4071fc4abf530af102ac6c7c429e1eaa1
Victor Iliya <VictorIliya15@gmail.com>
2024-09-04 14:35:18 +0100

    finished the styling for services, no contetn yet  placeholder

--- portfolio_1/src/App.js
+++ portfolio_1/src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/header/header"
 import Home from "./components/Home/Home" 
 import About from "./components/About/About"
 import Skills from "./components/skills/Skills"
+import Services from "./components/Services/Services" 
 function App() {
 
   return (
@@ -13,6 +14,7 @@ function App() {
         <Home/>
         <About/>
         <Skills/>
+        <Services/>
       </main>
     </>
   )
