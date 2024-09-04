502bb7f80a5aeb2b6e95f12825d2768b13200b55
Victor Iliya <VictorIliya15@gmail.com>
2024-09-04 18:44:58 +0100

    adding the experience sections

--- portfolio_1/src/components/Qualifications/Qualification.jsx
+++ portfolio_1/src/components/Qualifications/Qualification.jsx
@@ -3,7 +3,92 @@ import './qualification.css'
 
 const Qualification = () => {
   return (
-    <div>Qualification</div>
+    <section className="qualification section">
+        <h2 className="section__title">Qualifications</h2>
+        <span className="section__subtitle">My Personal Journey</span>
+
+        <div className="qualification__container container">
+            <div className="qualification__tabs">
+                <div className="qualification__button qualification__active button--flex">
+                    <i className="uil uil-graduation-cap qualification__icon"></i>Education
+                </div>
+
+                <div className="qualification__button button--flex">
+                    <i className="uil uil-briefcase-alt"></i>Experiencee
+                </div>
+            </div>
+
+            <div className="qualification__sections">
+                <div className="qualification__content">
+                    <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title">AWS Solutions</h3>
+                            <span className="qualification__subtitle">ABYAD</span>
+                            <div className="qualification__calendar">
+                                <i className="uil uil-calendar-alt"></i>
+                                2019 - 2022
+                            </div>
+                        </div>
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                    </div>
+
+
+                    <div className="qualification__data">
+                        <div className=""></div>
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                        <div>
+                            <h3 className="qualification__title">designer</h3>
+                            <span className="qualification__subtitle">ABYAD</span>
+                            <div className="qualification__calendar">
+                                <i className="uil uil-calendar-alt"></i>
+                                2019 - 2022
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                    <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title">AWS Solutions</h3>
+                            <span className="qualification__subtitle">ABYAD</span>
+                            <div className="qualification__calendar">
+                                <i className="uil uil-calendar-alt"></i>
+                                2019 - 2022
+                            </div>
+                        </div>
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                    </div>
+
+
+                    <div className="qualification__data">
+                        <div className=""></div>
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                        <div>
+                            <h3 className="qualification__title">designer</h3>
+                            <span className="qualification__subtitle">ABYAD</span>
+                            <div className="qualification__calendar">
+                                <i className="uil uil-calendar-alt"></i>
+                                2019 - 2022
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
