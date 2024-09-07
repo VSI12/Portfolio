6242fe0f175a16eede1c1bd4bd10b95472fd168e
Victor Iliya <VictorIliya15@gmail.com>
2024-09-07 11:26:33 +0100

    add the fotter links

--- portfolio_1/src/components/Home/Social.jsx
+++ portfolio_1/src/components/Home/Social.jsx
@@ -2,19 +2,19 @@
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="www.linkedin.com" className="home__social-icon" target="_blank">
+        <a href="https://.linkedin.com/in/victor-iliya" className="home__social-icon" target="_blank" rel="noreferrer">
             <i className="uil uil-linkedin-alt"></i>
         </a>
 
-        <a href="www.github.com" className="home__social-icon" target="_blank">
+        <a href="https://github.com/VSI12" className="home__social-icon" target="_blank" rel="noreferrer">
             <i className="uil uil-github-alt"></i>
         </a>
 
-        <a href="www.dribble.com" className="home__social-icon" target="_blank">
+        <a href="www.dribble.com" className="home__social-icon" target="_blank" rel="noreferrer">
             <i className="uil uil-dribbble"></i>
         </a>
 
-        <a href="www.instagram.com" className="home__social-icon" target="_blank">
+        <a href="www.instagram.com" className="home__social-icon" target="_blank" rel="noreferrer">
             <i className="uil uil-instagram"></i>
         </a>
     </div>
